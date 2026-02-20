53b3727ce23491dda10b92d52b6acc1adc4e7a0e
Marius Juston <Marius.juston@hotmail.fr>
2026-02-20 03:26:34 -0600

    removed unecesssary files

--- lib/C2000Ware_4_01_00_00/device_support/f2837xd/examples/cpu1/FinalProjectwithCANAprilStarter/cpu01/OptiTrack.h
+++
@@ -1,7 +0,0 @@
-#ifndef _OPTITRACK_H_
-#define _OPTITRACK_H_
-
-pose UpdateOptitrackStates(pose localROBOTps, int16_t * flag);
-#define FEETINONEMETER 3.28083989501312
-
-#endif
--- lib/C2000Ware_4_01_00_00/device_support/f2837xd/examples/cpu1/SPIRAMstarter/cpu01/matlab/list_serialports.m
+++
@@ -1,5 +0,0 @@
-function ports = list_serialports()
-% returns cell array of found serial ports under Win
-% uses CLI MODE command internally
-    [~,res]=system('mode');
-    ports=regexp(res,'COM\d+:','match')';
