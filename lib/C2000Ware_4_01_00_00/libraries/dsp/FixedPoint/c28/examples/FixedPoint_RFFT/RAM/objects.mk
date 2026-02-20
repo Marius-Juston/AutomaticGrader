cbe78e1ee485499c0997588f7c058ad24e1522d5
Marius Juston <Marius.juston@hotmail.fr>
2026-02-20 03:25:25 -0600

    removed unecesssary files

--- lib/C2000Ware_4_01_00_00/libraries/dsp/FixedPoint/c28/examples/FixedPoint_RFFT/RAM/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lc28x_fixedpoint_dsp_library_fpu32_eabi.lib -ldriverlib.lib -llibc.a
-
